46b9f065b138b41f9c8daab4e3df749261919f36
1054912 <120035639+1054912@users.noreply.github.com>
2022-12-07 15:31:48 +0000

    Update new-combine-data.R

--- amended_code/new-combine-data.R
+++ amended_code/new-combine-data.R
@@ -48,6 +48,6 @@ plot_df <- data.frame(
   average_rainfall = c(mean_rainfall_melb, mean_rainfall_ox))
 
 write.table(x = plot_df,
-            file = "clean_data/average-rainfallzz.csv",
+            file = "clean_data/average-rainfall.csv",
             sep = ",",
             row.names = FALSE)
